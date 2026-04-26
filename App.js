f2ce71131dafc7a6e84a69e98ad946f4f6338ef4
miguelzackk <mzsilvaw@gmail.com>
2026-04-26 16:21:06 -0300

    feat: aplicando api de CEP no mobile para testar

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import api from "./src/services/api";
+import {globalStyles} from "./src/styles/globalStyles";
+import {useEffect, useState} from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ScrollView, Text, TextInput, View} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [rua, setRua] = useState(null);
+    const [pesquisa, setPesquisa] = useState("");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        async function carregarRua() {
+            if (/^\d{8}$/.test(pesquisa)) {
+                try {
+                    const response = await api.get(pesquisa);
+                    setRua(response.data);
+                } catch (error) {
+                    console.error("Erro na requisição:", error.response?.data || error.message);
+                    setRua(null);
+                }
+            } else {
+                setRua(null);
+            }
+        }
+
+        carregarRua();
+    }, [pesquisa]);
+
+    return (<SafeAreaView style={globalStyles.container}>
+        <TextInput
+            style={globalStyles.input}
+            placeholder="Digite o CEP ou nome da rua"
+            value={pesquisa}
+            onChangeText={setPesquisa}
+        />
+
+        <Text style={globalStyles.titulo}>Resultado da pesquisa</Text>
+
+        <ScrollView>
+            {rua && (<View style={globalStyles.card}>
+                <Text>CEP: {rua.cep}</Text>
+                <Text>Estado: {rua.state}</Text>
+                <Text>Cidade: {rua.city}</Text>
+                <Text>Bairro: {rua.neighborhood}</Text>
+                <Text>Rua: {rua.street}</Text>
+            </View>)}
+        </ScrollView>
+    </SafeAreaView>);
+}
